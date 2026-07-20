43b2f077d391020aec5d02de4b5fc26356607971
fanice1998 <fanice0721@gmail.com>
2026-07-20 18:25:56 +0800

    Feat: zsh config

---
+++ zsh/install_plugin.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+# Tips about text history
+if [ ! -d fzf-tab ]; then
+  git clone https://github.com/Aloxaf/fzf-tab ~/.zsh/fzf-tab
+fi
+
+# Syntax highlighting
+if [ ! -d zsh-syntax-highlighting ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+fi
+
+mv ~/.zshrc ~/.zshrc.bak 2>/dev/null
+
+cp ./.zshrc.origin ~/.zshrc
+
+if command -v starship &>/dev/null; then
+  if ! grep -qF 'eval "$(starship init zsh)"' ~/.zshrc 2>/dev/null; then
+    echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+  fi
+fi
+
+source ~/.zshrc
