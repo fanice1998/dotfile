b5223f3bcd16e88cc89def95dc6786064180172e
fanice1998 <fanice0721@gmail.com>
2026-07-22 08:57:41 +0800

    Feat: add go environment for zsh

--- zsh/install_plugin.sh
+++ zsh/install_plugin.sh
@@ -14,12 +14,26 @@ mv ~/.zshrc ~/.zshrc.bak 2>/dev/null
 
 cp ./.zshrc.origin ~/.zshrc
 
-if command -v starship &>/dev/null; then
+# Go environment
+# ── Go environment ──────────────────────────────────────────────
+if command -v go >/dev/null 2>&1; then
+  if ! grep -qF 'GOROOT="$(go env GOROOT)"' ~/.zshrc 2>&1; then
+    echo 'export GOROOT="$(go env GOROOT)"' >> ~/.zshrc
+    echo 'export GOPATH="$(go env GOPATH)"' >> ~/.zshrc
+    echo 'export PATH="$PATH:$GOROOT/bin:$GOPATH/bin"' >> ~/.zshrc
+  fi
+fi
+
+# starship install and setting zshrc
+if command -v starship 2>&1; then
   if ! grep -qF 'eval "$(starship init zsh)"' ~/.zshrc 2>/dev/null; then
     echo 'eval "$(starship init zsh)"' >> ~/.zshrc
   fi
 else
   curl -sS https://starship.rs/install.sh | sh
+  echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 fi
 
+# reload
 source ~/.zshrc
+
