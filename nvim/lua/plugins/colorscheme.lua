06885db322c6a2db92cc095d41b7c3ca20eb0c57
fanice1998 <fanice0721@gmail.com>
2026-08-23 00:30:19 +0800

    Feat: update nvim colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,25 @@
 -- ~/.config/nvim/lua/plugins/colorscheme.lua
 return {
-  -- override everforest 的 opts (會覆蓋/lazyVim 預設的)
+  -- 關掉 tokyonight
+  { "folke/tokyonight.nvim", enabled = false },
+
+  -- 安裝 Everforest
   {
     "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "medium" -- soft / medium / hard
+      vim.g.everforest_enable_italic = 1
+      vim.g.everforest_better_performance = 1
+    end,
+  },
+
+  -- 關鍵：強制覆蓋 LazyVim 的預設主題
+  {
+    "LazyVim/LazyVim",
     opts = {
-      transparent = true,
-      style = "moon",
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      colorscheme = "everforest",
     },
   },
 }
