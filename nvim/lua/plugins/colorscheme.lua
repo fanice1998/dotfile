39ca8a9df638294a04143e6f7cc0c7f4ed00e817
fanice1998 <fanice0721@gmail.com>
2026-08-24 12:11:16 +0800

    Feat: add transparent background

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -12,6 +12,14 @@ return {
       vim.g.everforest_background = "medium" -- soft / medium / hard
       vim.g.everforest_enable_italic = 1
       vim.g.everforest_better_performance = 1
+
+      -- Enable transparent background
+      -- 1. Transparent background and sider.
+      -- 2. The background, sider, and floating windows are all transparent.
+      vim.g.everforest_transparent_background = 2
+
+      -- Apply color theme
+      vim.cmd.colorscheme("everforest")
     end,
   },
 
